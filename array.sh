4bb609be64efda16bfe1870ca46d63fd0035c74b
nirshang <ngodugunuri@gmail.com>
2024-12-31 20:10:55 -0700

    added

---
+++ array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+fruit=("mango" "banana" "grape" "apple")
+echo "first fruit is ${fruit[0]}"
+echo "second fruit is ${fruit[1]}"
+echo "third fruit is ${fruit[2]}"
+echo "fourth fruit is ${fruit[3]}"

267c7f5c6c8e920b1c87fce532f78ea27e51b9c0
nirshang <ngodugunuri@gmail.com>
2025-01-01 13:18:32 -0700

    added

--- array.sh
+++ array.sh
@@ -4,4 +4,4 @@ echo "first fruit is ${fruit[0]}"
 echo "second fruit is ${fruit[1]}"
 echo "third fruit is ${fruit[2]}"
 echo "fourth fruit is ${fruit[3]}"
-echo "all movies are ${fruit[@]}
+echo "all movies are ${fruit[@]}"
---
+++ special variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "All variables passed $@"
+echo "number of variables $#"
+echo "name of the script file: $0"
+echo "present working directory: 

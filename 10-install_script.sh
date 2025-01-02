fa75b4f6ca4d5dde31fcd1124928d621269108d6
nirshang <ngodugunuri@gmail.com>
2025-01-02 16:21:00 -0700

    LLL

--- 10-install_script.sh
+++ 10-install_script.sh
@@ -1,14 +1,58 @@
 #!/bin/bash
-set -x
+
 USERID=$(id -u)
-if [ $USERID -ne 0 ];then
-   echo "ERROR:to run this you must have sudo previlizes"
-   exit 1
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 
 fi
-dnf install mysql -y
-if [ $? -ne 0];then 
-   echo "mysql installing is Failure"
-   exit 1
-else 
-   echo "mysql installing is Success."
-fi 
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MySQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
+else
+    echo "MySQL is already ... INSTALLED"
+fi
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing MySQL ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing MySQL ... SUCCESS"
+# fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing Git ... FAILURE"
+        exit 1
+    else
+        echo "Installing Git ... SUCCESS"
+    fi
+else
+    echo "Git is already ... INSTALLED"
+fi
+
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing Git ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing Git ... SUCCESS"
+# fi
---
+++ sample.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -xv
+USERID=$(id -u)
+if [ $USER -ne 0 ];then
+   echo "ERROR:you have to have root previlises to run"
+   exit 1
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ];then
+   dnf install mysql -y
+   if [ $? -ne 0 ]
+   then
+      echo "installing mysql ...FAILURE"
+      exit 1
+    else
+       echo "installing mysql ...SUCCESS"
+    fi
+else 
+    echo "mysql is already installed"
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "installing git ...FAILURE"
+        exit 1
+    else
+    echo "installing git ....SUCCESS"
+    fi
+fi

38c3b3bf5613b0a262039af8c84bfb90b7ee63d5
nirshang <ngodugunuri@gmail.com>
2025-01-03 17:30:24 -0700

    ssh script

--- 13-logs.sh
+++ 13-logs.sh
@@ -19,26 +19,27 @@ VALIDATE(){
     fi
 }
 
-echo "script started exicuting at : $TIMESTAMP" &>>$LOG_FILE
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+echo "script started exicuting at : $TIMESTAMP" &>>$LOG_FILE_NAME
 
 if [ $USERID -ne 0 ];then
    echo "ERROR:you have to have root previlises to run"
    exit 1
 fi
 
-dnf list installed mysql &>>LOG_FILE
+dnf list installed mysql &>>LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-   dnf install mysql -y &>>LOG_FILE
+   dnf install mysql -y &>>LOG_FILE_NAME
    VALIDATE $? "installing mysql"
 else 
     echo -e "mysql is already ...... $Y INSTALLED"
 fi
 
-dnf list installed git &>>LOG_FILE
+dnf list installed git &>>LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-    dnf install git -y &>>LOG_FILE
+    dnf install git -y &>>LOG_FILE_NAME
     VALIDATE $? "installing git"
 else
     echo -e "git is already .... $Y INSTALLED"

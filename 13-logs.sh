217dce57cdb7c0ea6948adb314ebdc0eae677d18
nirshang <ngodugunuri@gmail.com>
2025-01-03 17:53:14 -0700

    ssh script

--- 13-logs.sh
+++ 13-logs.sh
@@ -7,28 +7,30 @@ Y="\e[33m"
 N="\e[34m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1 ) 
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S) 
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [  -$1 ne 0 ]
    then
-      echo -e  "$2 ... $R FAILURE"
+      echo -e  "$2 ... $R FAILURE $N"
       exit 1
     else
-       echo -e "$2 ... $G SUCCESS"
+       echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-echo "Script started exicuting at: $TIMSTAMP" &>>$LOG_FILE_NAME
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-if [ $USERID -ne 0 ];then
-   echo "ERROR:you have to have root previlises to run"
-   exit 1
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 
 fi
 
 dnf list installed mysql &>>LOG_FILE_NAME
+
 if [ $? -ne 0 ]
 then
    dnf install mysql -y &>>LOG_FILE_NAME
@@ -38,6 +40,7 @@ else
 fi
 
 dnf list installed git &>>LOG_FILE_NAME
+
 if [ $? -ne 0 ]
 then
     dnf install git -y &>>LOG_FILE_NAME

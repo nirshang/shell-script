45fad610cbc1f942552dce1821d7a579f20e39cd
nirshang <ngodugunuri@gmail.com>
2025-01-03 17:55:19 -0700

    ssh script

--- 13-logs.sh
+++ 13-logs.sh
@@ -1,10 +1,10 @@
- #!/bin/bash
-#set -xv
+#!/bin/bash
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[34m"
+N="\e[0m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
@@ -12,12 +12,12 @@ TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
-    if [  -$1 ne 0 ]
-   then
-      echo -e  "$2 ... $R FAILURE $N"
-      exit 1
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
     else
-       echo -e "$2 ... $G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
@@ -26,26 +26,26 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: You must have sudo access to execute this script"
-    exit 1 
+    exit 1 #other than 0
 fi
 
-dnf list installed mysql &>>LOG_FILE_NAME
+dnf list installed mysql &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
-then
-   dnf install mysql -y &>>LOG_FILE_NAME
-   VALIDATE $? "installing mysql"
-else 
-    echo -e "mysql is already ...... $Y INSTALLED"
-fi
-
-dnf list installed git &>>LOG_FILE_NAME
-
-if [ $? -ne 0 ]
-then
-    dnf install git -y &>>LOG_FILE_NAME
-    VALIDATE $? "installing git"
+then # not installed
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Installing MySQL"
 else
-    echo -e "git is already .... $Y INSTALLED"
-    
+    echo -e "MySQL is already ... $Y INSTALLED $N"
+fi
+
+
+dnf list installed git &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Installing Git"
+else
+    echo -e "Git is already ... $Y INSTALLED $N"
 fi

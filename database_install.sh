fada19b03541402ba026781a8a2b7941a014d6ac
nirshang <ngodugunuri@gmail.com>
2025-01-27 22:48:15 -0700

    l

--- database_install.sh
+++ database_install.sh
@@ -46,6 +46,3 @@ VALIDATE $? "Enabling mysql server"
 
 systemctl start mysqld &>>$LOG_FILE_NAME
 VALIDATE $? "Starting mysql server"
-
-#mysql_secure_installation --set-root-pass ExpenseApp@1
-#VALIDATE $? "setting root password"
